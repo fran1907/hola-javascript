11eccbbf5c11bc5cf725c0bfe913de07e2506d6a
fran1907 <frm1907.iescc@gmail.com>
2025-12-17 13:20:30 +0100

    Nuevo Repositorio

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,53 @@
+//Tipos de datos primitivos
+
+//Cadenas de texto (String)
+
+let name = "Francisco Ruiz"
+let alias = 'Fran'
+let email = `fran@gmail.com`
+
+
+//Numeros (number)
+
+let age = 37 //Entero
+let height = 1.77 //Decimal
+
+//Booleans (boolean)
+
+let esProfesor = true
+let esEstudiante = false
+
+//Undefine d
+
+let undefinedValue
+console.log(undefinedValue)
+
+//Null
+
+let nullValue  = null
+
+//Symbol
+
+let mySymbol = Symbol("mySymbol")
+
+//BigInt
+
+let myBigInt = BigInt(734825844389547855485874387972298129031344923723432)
+
+//Mostramos los tipos de datos
+
+console.log(typeof name)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+
+
+console.log(typeof esProfesor)
+console.log(typeof esEstudiante)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof myBigInt)
